9264184cc3af5ffb65aa0b4657485b0f21ac2494
loverofMush <tolujamal1@gmail.com>
2021-11-24 23:50:03 +0100

    - minor spelling fix

--- customers/src/service/app.service.ts
+++ customers/src/service/app.service.ts
@@ -4,18 +4,18 @@ import CustomersDao from "../utils/dao";
 /** Service to handle database communication
  * This pattern allows for separation of concern and lets the service focus on 
  * calling the Customers Data Acccess Object instead of calling the database directly
- * @return new AppService()
+ * @returns new AppService()
  */
 class AppService implements CRUD {
     /** Get All customers from the database
-     * @return customers
+     * @returns customers
      */
     async getAll(limit: number, page: number) {
         return CustomersDao.getCustomers()
     };
 
     /** Add a new Customer
-     * @return customer.id
+     * @returns customer.id
      */
     async create(resource: any) {
         return CustomersDao.addCustomer(resource)
