2a00bbd00789a7373c64d0345aac22e8246b7ebf
loverofMush <tolujamal1@gmail.com>
2021-11-24 23:39:27 +0100

    - added doc string for class methods

--- customers/src/service/app.service.ts
+++ customers/src/service/app.service.ts
@@ -4,12 +4,19 @@ import CustomersDao from "../utils/dao";
 /** Service to handle database communication
  * This pattern allows for separation of concern and lets the service focus on 
  * calling the Customers Data Acccess Object instead of calling the database directly
+ * @return new AppService()
  */
 class AppService implements CRUD {
+    /** Get All customers from the database
+     * @return customers
+     */
     async getAll(limit: number, page: number) {
         return CustomersDao.getCustomers()
     };
 
+    /** Add a new Customer
+     * @return customer.id
+     */
     async create(resource: any) {
         return CustomersDao.addCustomer(resource)
     };
