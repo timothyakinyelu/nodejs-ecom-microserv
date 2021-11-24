0a525ca0379471353979f950cf6e0a4efea10ca3
loverofMush <tolujamal1@gmail.com>
2021-11-24 23:29:41 +0100

    - added jsdoc

--- customers/src/service/app.service.ts
+++ customers/src/service/app.service.ts
@@ -1,6 +1,10 @@
 import { CRUD } from "../utils/crud";
 import CustomersDao from "../utils/dao";
 
+/** Service to handle database communication
+ * This pattern allows for separation of concern and lets the service focus on 
+ * calling the Customers Data Acccess Object instead of calling the database directly
+ */
 class AppService implements CRUD {
     async getAll(limit: number, page: number) {
         return CustomersDao.getCustomers()
