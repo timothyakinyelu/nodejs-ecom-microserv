2a44120623bbca93406805c6ddbe33fdaff2e887
loverofMush <tolujamal1@gmail.com>
2021-11-25 22:52:32 +0100

    - added CreateCustomerDto interface

---
+++ customers/src/utils/dto.ts
@@ -0,0 +1,6 @@
+/** Data Transfer Object that serves as a model for data to be transferred and received
+ * from the customer database
+ */
+export interface CreateCustomerDto {
+    name: string;
+}
