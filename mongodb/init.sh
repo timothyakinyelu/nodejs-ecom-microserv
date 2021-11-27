68560f5c8206921f25e1ca3f25e1014d517cc28c
loverofMush <tolujamal1@gmail.com>
2021-11-28 00:22:09 +0100

    - updated init.sh

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 mongoimport --db customerdb --collection customers --type json --file customerdata.json --jsonArray --uri "mongodb://mongo:27017"
+mongoimport --db productdb --collection products --type json --file productdata.json --jsonArray --uri "mongodb://mongo:27017"
