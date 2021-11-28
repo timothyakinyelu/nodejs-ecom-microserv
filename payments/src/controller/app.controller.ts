9d77351e7b7be3235cda1f2dbe2f8fb0613a5694
loverofMush <tolujamal1@gmail.com>
2021-11-29 00:52:42 +0100

    - imported sendRabbitMsg

--- payments/src/controller/app.controller.ts
+++ payments/src/controller/app.controller.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import debug from 'debug';
 import appService from '../service/app.service';
+import sendRabbitMsg from '../utils/producer';
 import { CreateTransactionDto } from '../utils/dto';
 
 const log: debug.IDebugger = debug('app:app-controller');
@@ -22,11 +23,21 @@ class AppController {
     async addTransaction(payload: CreateTransactionDto) {
         const transaction = await appService.create(payload);
         log(`Return the saved transaction ${transaction}`);
-        // res.status(201).send({ id: transactionId });
+        return { message: 'Transaction saved!' };
     }
 
-    async receiveTransaction(req: express.Request, res: express.Response) {
-        // parse the request body and send to a rabbitmq queue
+    /** Receive the order and initiate payment request 
+     * send details of order to rabbitMq queue if payment
+     * is successful
+     */
+    async receiveOrder(req: express.Request, res: express.Response) {
+        let payload = req.body;
+        log(`Received payload of ${payload} and initiated payment request`);
+
+        // in a real world application the rabbitMQ msg will not be sent until
+        // a response is received from the payment gateway being used
+        sendRabbitMsg('insertTransaction', JSON.stringify(payload));
+        return res.status(200).json({ message: "Payment successful!" });
     }
 }
 
