e2fbebcaa6dcfd34070c8b040cd361e3240ebaf9
loverofMush <tolujamal1@gmail.com>
2021-12-01 02:15:25 +0100

    - changed localhost to docker service names

--- gateway/src/controller/app.controller.ts
+++ gateway/src/controller/app.controller.ts
@@ -9,37 +9,37 @@ const log: debug.IDebugger = debug('app:app-controller');
   */
 class AppController {
     async sendToCustomers(req: express.Request, res: express.Response) {
-        const { status, data } = await axios.post('http://localhost:3301/customers', req.body);
+        const { status, data } = await axios.post('http://customer-api/customers', req.body);
         res.status(status).send({ id: data });
     }
 
     async getFromCustomers(req: express.Request, res: express.Response) {
-        const { status, data } = await axios.get('http://localhost:3301/customers');
+        const { status, data } = await axios.get('http://customer-api/customers');
         res.status(status).send(data);
     }
 
     async sendToOrders(req: express.Request, res: express.Response) {
-        const { status, data } = await axios.post('http://localhost:3303/orders', req.body);
+        const { status, data } = await axios.post('http://order-api/orders', req.body);
         res.status(status).send({ order: data });
     }
 
     async getFromOrders(req: express.Request, res: express.Response) {
-        const { status, data } = await axios.get('http://localhost:3303/orders');
+        const { status, data } = await axios.get('http://order-api/orders');
         res.status(status).send(data);
     }
 
     async sendToProducts(req: express.Request, res: express.Response) {
-        const { status, data } = await axios.post('http://localhost:3302/products', req.body);
+        const { status, data } = await axios.post('http://product-api/products', req.body);
         res.status(status).send({ product: data });
     }
 
     async getFromProducts(req: express.Request, res: express.Response) {
-        const { status, data } = await axios.get('http://localhost:3302/products');
+        const { status, data } = await axios.get('http://product-api/products');
         res.status(status).json(data)
     }
 
     async getFromPayments(req: express.Request, res: express.Response) {
-        const { status, data } = await axios.get('http://localhost:3304/transactions');
+        const { status, data } = await axios.get('http://payment-api/transactions');
         res.status(status).send(data);
     }
 
